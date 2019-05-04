06cfe8d7f2ca846768d50ad6f411e1ef590c7109
Amy Em <amy@systemexperts.asia>
2019-05-05 01:30:28 +0700

    [REFACTOR] gobal footer

--- Contact.php
+++ Contact.php
@@ -69,25 +69,7 @@
 
   </main>
 
-    <footer>
-        <div class="row footer" style="height:80px">
-          <div class="col-sm-4 text-center" style="background-color:lavender;">
-            <a href="Contact.php" class="nav-link text-dark">Contact Us</a>
-          </div>
-          <div class="col-sm-4 text-center">
-                <button type="button" class="btn btn-primary">Switch to dark mode</button>
-          </div>
-          <div class="col-sm-4 text-center" style="background-color:lavender;">
-            <a href="#" class="fa fa-facebook"></a>
-            <a href="#" class="fa fa-twitter"></a>
-            <a href="#" class="fa fa-linkedin"></a>
-            <a href="#" class="fa fa-instagram"></a>
-          </div>
-        </div>
-        <div class="row1">
-          <div class="col-sm-12 text-center">@2019 Copyright: Em Thavrak</div>
-        </div>
-    </footer>
+    <?php include "Footer.php"; ?>
 
   </body>
 </html>
