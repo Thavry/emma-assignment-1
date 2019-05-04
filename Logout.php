0c5188a4be21016e2575a8f44e4982a24c2bdbbe
Amy Em <amy@systemexperts.asia>
2019-05-05 00:56:32 +0700

    [REFACTOR] logout function

--- Logout.php
+++ Logout.php
@@ -1,10 +1,7 @@
 <?php
-    if(isset($_POST['logout']))
-    {
-       session_start();
-       session_destroy();
-       unset($_SESSION['USERNAME']);
-       header("location: ".$_SERVER['HTTP_REFERER']);
-       exit;
-    }
+   session_start();
+   session_destroy();
+   unset($_SESSION['USERNAME']);
+   header("location: ".$_SERVER['HTTP_REFERER']);
+   exit;
 ?>
